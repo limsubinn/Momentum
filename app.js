7f33bdbbd53cf8c1f17f6fefbbf0c4abd2cc94f7
limsubinn <mymyx02@naver.com>
2023-01-10 14:30:46 +0900

    #3.8 JAVASCRIPT ON THE BRWOSER - CSS in Javascript part Three

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
-const title = document.querySelector(".hello h1")
+const h1 = document.querySelector(".hello h1")
 
-title.innerText = "Got you!"
-
+/* css 이용해서 바꾸기 (권장) */
 function handleTitleClick() {
-    title.style.color = "blue"
+    h1.classList.toggle("clicked")
+    // const clickedClass = "clicked"
+
+    // if (h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass)
+    // } else {
+    //     h1.classList.add(clickedClass)
+    // }
 }
 
-title.addEventListener("click", handleTitleClick)
+h1.addEventListener("click", handleTitleClick)
 
+/* javascript 이용해서 바꾸기 */
+function handleMouseEnter() {
+    h1.innerText = "Mouse in"
+}
+
+function handleMouseLeave() {
+    h1.innerText = "Mouse out"
+}
+
+h1.addEventListener("mouseenter", handleMouseEnter)
+h1.onmouseleave = handleMouseLeave
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato"
+}
+
+function handleWindowCopy() {
+    alert("copier!!!")
+}
+
+function handleWindowOffline() {
+    alert("no WIFI")
+}
+
+function handleWindowOnline() {
+    alert("good")
+}
+
+window.addEventListener("resize", handleWindowResize)
+window.addEventListener("copy", handleWindowCopy)
+window.addEventListener("offline", handleWindowOffline)
+window.addEventListener("online", handleWindowOnline)
